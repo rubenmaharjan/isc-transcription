e137d114da5512aedd001e5afc1669637e43ffd3
Mofeoluwa <42873366+Mofeoluwa@users.noreply.github.com>
2023-04-16 22:22:37 -0400

    Add updates according to week 12's correction

--- isc_file_search.py
+++ isc_file_search.py
@@ -1,92 +1,105 @@
 import os
+import logging
+
+"""
+This file references the os module, which provides a way to interact with the operating system, including
+file and directory manipulation.
+"""
 
 class IscFileSearch:
     def __init__(self, path):
+        """
+        Initializes the IscFileSearch class with a path attribute and a logger attribute from the logging module.
+        """
         self.path = path
-        
-    def traverse_directory(self):
+        self.logger = logging.getLogger(__name__)
+
+    
+    def traverse_directory(self, path=None):
         """
         Recursively traverses a directory and returns a list of file paths for all
         files that end with .mp3 or .wav.
         """
-        if not os.path.exists(self.path):
-            print("Directory does not exist:", self.path)
+        if path is None:
+            path = self.path
+        
+        if not os.path.exists(path):
+            self.logger.error("Directory does not exist: %s", path)
             return []
 
         file_paths = []
         
-        for file_name in os.listdir(self.path):
-            file_path = os.path.join(self.path, file_name)
-            
-            if os.path.isdir(file_path):
-                subdir = IscFileSearch(file_path)
-                file_paths.extend(subdir.traverse_directory())
+        for dirpath, dirnames, filenames in os.walk(path, onerror=self.logger.error):
+            for filename in filenames:
+                file_path = os.path.join(dirpath, filename)
                 
-            elif os.path.isfile(file_path):
                 if file_path.lower().endswith(('.mp3', '.wav')):
                     file_paths.append(file_path)
                 
         return file_paths
     
-    def get_file(self):
+    def get_files(self, file_exts=['mp3', 'wav']):
         """
-        Returns the path of the first file found in the directory that ends with .mp3 or .wav.
+        Returns a list of file paths in the directory whose extensions are in the specified file_exts parameter.
         """
         if not os.path.exists(self.path):
-            print("Directory does not exist:", self.path)
-            return None
-        
-        for file_name in os.listdir(self.path):
-            file_path = os.path.join(self.path, file_name)
-            
-            if os.path.isfile(file_path) and file_path.lower().endswith(('.mp3', '.wav')):
-                return file_path
-            
-        print("No files found in directory:", self.path)
-        return None
+            self.logger.error("Directory does not exist: %s", self.path)
+            return []
     
+        return [os.path.join(self.path, file) for file in os.listdir(self.path) 
+                if os.path.isfile(os.path.join(self.path, file)) 
+                and file.lower().rsplit('.')[-1] in file_exts]
+
     def delete_file(self, file_path):
         """
         Deletes a file at the specified path.
         """
         if not os.path.exists(file_path):
-            print("File does not exist:", file_path)
+            self.logger.error("File does not exist: %s", file_path)
             return
         
         os.remove(file_path)
-        print("File deleted:", file_path)
+        self.logger.info("File deleted: %s", file_path)
     
     def rename_file(self, old_name, new_name):
         """
-        Renames a file in the directory from old_name to new_name.
+        Renames a file in the directory from old_name to new_name and returns a status code.
         """
         old_path = os.path.join(self.path, old_name)
         new_path = os.path.join(self.path, new_name)
-        
+
         if not os.path.exists(old_path):
-            print("File does not exist:", old_path)
-            return
-        
-        os.rename(old_path, new_path)
-        print("File renamed from", old_name, "to", new_name)
-    
+            self.logger.error("File does not exist: %s", old_path)
+            return 1
+
+        if os.path.exists(new_path):
+            self.logger.warning("File already exists with new name: %s", new_name)
+            return 2
+
+        try:
+            os.rename(old_path, new_path)
+            self.logger.info("File renamed from %s to %s", old_name, new_name)
+            return 0
+
+        except OSError as e:
+            self.logger.error("Failed to rename file %s to %s: %s", old_name, new_name, str(e))
+            return 3
+          
     def get_file_properties(self, file_path):
         """
         Returns a dictionary of properties for the file at the specified path, including
         file name, file size, creation time, modified time, and access time.
         """
         if not os.path.exists(file_path):
-            print("File does not exist:", file_path)
+            self.logger.error("File does not exist: %s", file_path)
             return None
         
         file_stats = os.stat(file_path)
         
-        properties = {
+        return {
             "file_name": os.path.basename(file_path),
             "file_size": file_stats.st_size,
             "creation_time": file_stats.st_ctime,
             "modified_time": file_stats.st_mtime,
             "access_time": file_stats.st_atime,
         }
-        
-        return properties
