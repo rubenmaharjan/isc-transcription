f1ecf24cd03c49e86bf415a73a1c7b347fc2f80d
Bigya Bajracharya <bigya@Bigyas-MacBook-Pro.local>
2023-04-23 23:39:42 -0400

    Resolve conflicts and add config code to main

--- tests/test_transcriptionConfig.py
+++ tests/test_transcriptionConfig.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 import xml.etree.ElementTree as ET
-from src.config_file.TranscriptionConfig import TranscriptionConfig
+from src.utils.TranscriptionConfig import TranscriptionConfig
 
 
 class TestTranscriptionConfig(unittest.TestCase):
