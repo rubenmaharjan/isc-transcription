a9c737f7f33c875eda7dc6087709fb4dd4e11167
Bigya <bigyabajracharya@gmail.com>
2023-11-07 01:22:47 -0500

    [MAIN] Add args to support more cmd args and initiliaze logger at the start of program

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from src.utils.ISCLogWrapper import ISCLogWrapper, logging
 from src.transcribe.TranscribeFactory import TranscribeFactory
 from src.utils.TranscriptionConfig import TranscriptionConfig
 
-default_audio='sample/Sample.mp3'
+default_audio = 'sample/Sample.mp3'
+
 
 def setup_logging():
     isc_log_wrapper = ISCLogWrapper(
@@ -24,15 +25,30 @@ def setup_logging():
         return 1
     return logging.getLogger(__name__)
 
+
+logger = setup_logging()
+
+
 def parse_command_line_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--audio", help="Specify the input audio file")
-    parser.add_argument("--configxml", help="Specify the input xml config file")
+    parser.add_argument(
+        "--configxml", help="Specify the input xml config file")
 
     # new arguments
-    parser.add_argument("--model_type", help="Specify the model type for transcription")
- 
+    parser.add_argument(
+        "--model_type", help="Specify the model type for transcription")
+    parser.add_argument(
+        "--audiodir", help="Specify the directory of audio to transcribe")
+    parser.add_argument("--transcriptiondir",
+                        help="Specify the directory to store transcriptions")
+    parser.add_argument(
+        "--hf_token", help="Specify the user token needed for diarization")
+    parser.add_argument(
+        "--extensions", help="List of audio extensions in audiodir")
+
     return parser.parse_args()
+
 
 def get_audio_source(config, cmd_audio, cmd_config, logger):
     if cmd_audio:
@@ -43,22 +59,27 @@ def get_audio_source(config, cmd_audio, cmd_config, logger):
         try:
             extract_config = TranscriptionConfig(cmd_config)
             if 'audiodir' in extract_config:
-                logger.info('Starting transcription with cmd_config audio directory')
+                logger.info(
+                    'Starting transcription with cmd_config audio directory')
                 return extract_config.get('audiodir')
             else:
-                logger.error("The configuration file does not contain 'audiodir'.")
+                logger.error(
+                    "The configuration file does not contain 'audiodir'.")
         except Exception as e:
             logger.error("Error while reading the configuration file:", e)
     else:
-        logger.error("Wrong file name or extension for the configuration file.")
+        logger.error(
+            "Wrong file name or extension for the configuration file.")
 
     default_audio = config.get('audiodir')
     if default_audio:
         logger.info('Starting transcription with default audio directory')
         return default_audio
 
-    logger.error("No valid audio directory specified. Transcription cannot proceed.")
-    return  default_audio
+    logger.error(
+        "No valid audio directory specified. Transcription cannot proceed.")
+    return default_audio
+
 
 def validate_configxml(xml_file, xsd_file):
     # Load the XML file
@@ -71,36 +92,34 @@ def validate_configxml(xml_file, xsd_file):
     # Load the XML schema
     schema = etree.XMLSchema(file=xsd_file)
 
+    logger.info('Validating to check if the xml meets schema requirements')
+
     # Validate the XML document against the schema
     try:
         schema.assertValid(xml_doc)
-        print("XML document is valid according to the schema.")
+        logger.info("XML document is valid according to the schema.")
     except Exception as e:
-        print("XML document is not valid according to the schema.")
-        print(e)
-
+        logger.error("XML document is not valid according to the schema.", e)
 
 
 def main():
-    logger = setup_logging()
-    
     path1 = os.getcwd()
     path = os.path.join(path1, "config/dev_config.xml")
     config = TranscriptionConfig(path)
-    
+
     validate_configxml("config/dev_config.xml", "./config_validator.xsd")
     args = parse_command_line_args()
     audio = get_audio_source(config, args.audio, args.configxml, logger)
-    
+
     hf_token = config.get('settings/hf_token')
     if audio:
         logger.info("Starting Transcription.")
         logger.info("CWD: " + os.getcwd())
-        model = WhisperxTranscriber("small", hf_token, audio)  
+        model = WhisperxTranscriber("small", hf_token, audio)
         model.transcribe()
     else:
         logger.error("Cannot access audio file")
 
 
 if __name__ == '__main__':
-     main()
+    main()
