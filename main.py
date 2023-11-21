c0df802f0f7ce377e214c22fed7bea5de6e5cc11
Mofeoluwa <42873366+Mofeoluwa@users.noreply.github.com>
2023-11-21 16:17:56 -0500

    header comment update

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 # *************************************************************************************************************************
 #   main.py 
-#       This is the entry point for the transcription application using the WhisperxTranscriber. It handles command-line 
-#       arguments, sets up logging, validates XML configuration, and initiates the transcription process.
+#       This script serves as the entry point for the audio transcription application, incorporating the WhisperxTranscriber
+#       for processing audio files. It utilizes command-line argument handling, logging via ISCLogWrapper, XML configuration
+#       validation with TranscriptionConfig, and file searching with IscFileSearch.
 # -------------------------------------------------------------------------------------------------------------------
 #   Usage:
 #      python main.py --audio [path_to_audio_file] --configxml [path_to_config_file] [Other_Arguments]
 #      Arguments and defaults:
-#         -a, --audio [path] : Specify the path to the input audio file to be transcribed.
-#         -cx, --configxml [path] : Specify the path to the XML configuration file. Default 'config/default_config.xml'
-#         -mt, --model_type [type] : Specify the Whisper model type for transcription. Default 'base'
-#         -ad, --audiodir [dir] : Specify the directory of audio files to transcribe. Default 'audio_files'
-#         -td, --transcriptiondir [dir] : Specify the directory to store transcriptions. Default 'transcriptions'
-#         -ht, --hf_token [token] : Hugging Face authentication token for using models with diarization.
-#         -e, --extensions [ext] : List of audio file extensions in audiodir. Default ['.mp3', '.wav', '.aac']
+#         -a, --audio [path] : Path to the audio file or directory for transcription.
+#         -cx, --configxml [path] : XML configuration file. Default is 'config/default_config.xml'
+#         -mt, --model_type [type] : Whisper model type for transcription. Defaults to 'base'.
+#         -ad, --audiodir [dir] : Directory containing audio files for transcription. Defaults to 'audio_files'.
+#         -td, --transcriptiondir [dir] : Directory to store transcription files. Defaults to 'transcriptions'.
+#         -ht, --hf_token [token] : Hugging Face token for model access with diarization.
+#         -e, --extensions [ext] : List of allowed audio file extensions. Defaults to ['.mp3', '.wav', '.aac'].
 #      Outputs:
-#         Initiates the transcription process and outputs transcription files in the specified directory.
-#         Generates logs of the application's activities and errors.
+#         Processes and transcribes audio files, outputting transcription files in the specified directory.
+#         Application activity and errors are logged.
 # ---------------------------------------------------------------------------------------------------------------------
 #   TODO:
-#   -.  Improve error handling for command-line argument parsing and XML validation.
-#   -.  Extend the application to handle different input/output formats and sources.
-#   -.  Refactor the code for better modularity and testability.
+#   -.  Enhance error handling for argument parsing and configuration validation.
+#   -.  Expand support for various input/output formats and sources.
+#   -.  Refactor for improved modularity and testability.
 # ---------------------------------------------------------------------------------------------------------------------
 #   last updated:  November 2023
 #   authors:       Ruben Maharjan, Bigya Bajarcharya, Mofeoluwa Jide-Jegede
@@ -29,8 +30,9 @@
 # imports
 # ***********************************************
 
-# os - for file path operations
-#   getcwd - get the current working directory
+# os - module providing a portable way of using operating system dependent functionality
+#   os.path - submodule of os for manipulating paths
+#   os.path.isdir, os.path.isfile - functions to check if a path is a directory or a file
 
 # Custom packages for the transcription model and utilities
 # src.transcribe.models.WhisperxTranscriber - custom package for the transcription model
@@ -42,6 +44,9 @@
 
 # src.utils.TranscriptionConfig - custom configuration handler for transcription settings
 #   TranscriptionConfig - class to manage transcription configuration from an XML file
+
+# src.utils.IscFileSearch - module for searching and handling files within directories
+#   IscFileSearch - class for searching files and performing file operations in a specified directory
 
 import os
 from src.transcribe.models.WhisperxTranscriber import WhisperxTranscriber
@@ -83,7 +88,7 @@ def main():
     if os.path.isdir(audio_path):
         # Use IscFileSearch to get all audio files in the directory
         file_search = IscFileSearch(audio_path)
-        
+
         # Use the traverse_directory method if it's a directory
         audio_files = file_search.traverse_directory()  
     elif os.path.isfile(audio_path):
@@ -91,18 +96,18 @@ def main():
         audio_files.append(audio_path)
     else:
         logger.error("The specified audio path is neither a file nor a directory.")
-        return  # Exit if the path is invalid
+        return 
 
     if not audio_files:
         logger.error("No audio files found in the specified path.")
-        return  # Exit if no audio files are found
+        return  
 
     if hf_token:
         for audio in audio_files:
             logger.info(f"Starting transcription for {audio}")
             # Instantiate the WhisperxTranscriber model with the audio file
             model = WhisperxTranscriber(model_size="small", hf_token=hf_token, audio_files=audio, output_dir=transcription_dir)
-            model.transcribe()  # Transcribe the audio file
+            model.transcribe() 
     else:
         logger.error("HF Token not provided.")
 
